9711a9d8365d7b18bbaf15da23ef7921a0be48dd
Michal Grzelczak <mzg.kgb@gmail.com>
2019-05-30 12:34:58 +0200

    Updates

---
+++ precache-manifest.180ad2fc11ea3bf4f8a875ad9fb62ec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4d9ed93156e78954d64e55ddb05b98",
+    "url": "/blinksoftware/index.html"
+  },
+  {
+    "revision": "9a4d86e856147e696e38",
+    "url": "/blinksoftware/static/css/main.e2dc2730.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/blinksoftware/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "9a4d86e856147e696e38",
+    "url": "/blinksoftware/static/js/main.f5ac761f.chunk.js"
+  },
+  {
+    "revision": "6f2376f2194e8d8ba880",
+    "url": "/blinksoftware/static/js/runtime~main.e72da6cb.js"
+  }
+]);
